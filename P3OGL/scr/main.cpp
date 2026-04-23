3266b51fa43584112b8ec082426881850b47a781
CristianInBits <128726794+CristianInBits@users.noreply.github.com>
2026-04-23 22:13:04 +0200

    Esqueleto inicial

---
+++ P3OGL/scr/main.cpp
@@ -0,0 +1,100 @@
+#include "BOX.h"
+#include "auxiliar.h"
+
+
+#include <gl/glew.h>
+#define SOLVE_FGLUT_WARNING
+#include <gl/freeglut.h> 
+
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <iostream>
+
+
+//////////////////////////////////////////////////////////////
+// Datos que se almacenan en la memoria de la CPU
+//////////////////////////////////////////////////////////////
+
+//Matrices
+glm::mat4	proj = glm::mat4(1.0f);
+glm::mat4	view = glm::mat4(1.0f);
+glm::mat4	model = glm::mat4(1.0f);
+
+
+//////////////////////////////////////////////////////////////
+// Variables que nos dan acceso a Objetos OpenGL
+//////////////////////////////////////////////////////////////
+//Por definir
+
+
+//////////////////////////////////////////////////////////////
+// Funciones auxiliares
+//////////////////////////////////////////////////////////////
+//!!Por implementar
+
+//Declaración de CB
+void renderFunc();
+void resizeFunc(int width, int height);
+void idleFunc();
+void keyboardFunc(unsigned char key, int x, int y);
+void mouseFunc(int button, int state, int x, int y);
+
+//Funciones de inicialización y destrucción
+void initContext(int argc, char** argv);
+void initOGL();
+void initShader(const char *vname, const char *fname);
+void initObj();
+void destroy();
+
+
+//Carga el shader indicado, devuele el ID del shader
+//!Por implementar
+GLuint loadShader(const char *fileName, GLenum type);
+
+//Crea una textura, la configura, la sube a OpenGL, 
+//y devuelve el identificador de la textura 
+//!!Por implementar
+unsigned int loadTex(const char *fileName);
+
+
+int main(int argc, char** argv)
+{
+	std::locale::global(std::locale("spanish"));// acentos ;)
+
+	initContext(argc, argv);
+	initOGL();
+	initShader("../shaders_P3/shader.v0.vert", "../shaders_P3/shader.v0.frag");
+	initObj();
+
+	destroy();
+
+	return 0;
+}
+	
+//////////////////////////////////////////
+// Funciones auxiliares 
+void initContext(int argc, char** argv){}
+void initOGL(){}
+void destroy(){}
+void initShader(const char *vname, const char *fname){}
+void initObj(){}
+
+GLuint loadShader(const char *fileName, GLenum type){ return 0; }
+unsigned int loadTex(const char *fileName){ return 0; }
+
+void renderFunc(){}
+void resizeFunc(int width, int height){}
+void idleFunc(){}
+void keyboardFunc(unsigned char key, int x, int y){}
+void mouseFunc(int button, int state, int x, int y){}
+
+
+
+
+
+
+
+
+
